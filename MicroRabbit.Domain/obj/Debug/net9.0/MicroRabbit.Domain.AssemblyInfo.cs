12f2424488fafeafdac1ab9f25c87eb9b1aa6b29
serkanyurttapan <serkan.yurttapan@farmasi.com.tr>
2025-03-28 21:54:02 +0300

    initial commit

--- MicroRabbit.Domain/obj/Debug/net9.0/MicroRabbit.Domain.AssemblyInfo.cs
+++ MicroRabbit.Domain/obj/Debug/net9.0/MicroRabbit.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicroRabbit.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a878537d66c920f781fd0772ae6eabc17de632")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicroRabbit.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicroRabbit.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
